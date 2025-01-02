be6fcddc74dcc7789b1e9b5eb09ded43b6dfcb8b
Bao44 <tqbao44@gmail.com>
2025-01-02 20:01:51 +0700

    completed header

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <header>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
       <h2>Hello Word</h2>
     </div>
   );
