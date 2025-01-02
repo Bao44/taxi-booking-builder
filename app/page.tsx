df9298fba9b08c00af126306152be48d857bf5bf
Bao44 <tqbao44@gmail.com>
2025-01-02 21:03:31 +0700

    implement component booking

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Booking from "@/components/Booking/Booking";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h2>Hello Word</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className=""><Booking /></div>
+        <div className="col-span-2 bg-red-100">Map</div>
+      </div>
     </div>
   );
 }
