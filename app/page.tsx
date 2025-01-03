d20332969c4f64fa6f4929e548fedd7719016ea3
Bao44 <tqbao44@gmail.com>
2025-01-03 11:36:00 +0700

    implement User Location

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,37 @@
-"use client"
+"use client";
 import Booking from "@/components/Booking/Booking";
 import MapBoxMap from "@/components/Map/MapBoxMap";
+import { UserLocationContext } from "@/context/UserLocationContext";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [userLocation, setUserLocation] = useState<any>();
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      setUserLocation({
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      });
+    });
+  };
   return (
     <div className="">
-      <div className="grid grid-cols-1 md:grid-cols-3">
-        <div className=""><Booking /></div>
-        <div className="col-span-2"><MapBoxMap /></div>
-      </div>
+      <UserLocationContext.Provider value={{userLocation, setUserLocation}}>
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          <div className="">
+            <Booking />
+          </div>
+          <div className="col-span-2">
+            <MapBoxMap />
+          </div>
+        </div>
+      </UserLocationContext.Provider>
     </div>
   );
 }
