8baa7c0f4a5e6bb002133ad8e3c4d9ab4545cdce
Bao44 <tqbao44@gmail.com>
2025-01-03 21:15:41 +0700

    fix booking

--- components/Booking/Booking.tsx
+++ components/Booking/Booking.tsx
@@ -1,4 +1,43 @@
-import React, { useContext, useState } from "react";
+// "use client"
+// import React, { useContext, useState } from "react";
+// import AutocompleteAddress from "./AutocompleteAddress";
+// import Cars from "./Cars";
+// import Cards from "./Cards";
+// import { useRouter } from "next/navigation";
+// import { SelectedCarAmountContext } from "@/context/SelectedCarAmountContext";
+
+// function Booking() {
+//   const screenHight = window.innerHeight * 0.8;
+//   const { carAmount, setCarAmount } = useContext(SelectedCarAmountContext);
+//   const router: any = useRouter();
+//   return (
+//     <div className="p-5">
+//       <h2 className="text-[20px] font-semibold">Booking</h2>
+//       <div
+//         className="border-[1px] p-5 rounded-md"
+//         style={{ height: screenHight }}
+//       >
+//         <AutocompleteAddress />
+//         <Cars />
+//         <Cards />
+//         <button
+//           className={`w-full bg-yellow-400 p-1 rounded-md mt-4 ${
+//             !carAmount ? "bg-gray-200" : null
+//           }`}
+//           onClick={() => router.push("/payment")}
+//         >
+//           Book
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Booking;
+
+
+"use client"
+import React, { useContext, useState, useEffect } from "react";
 import AutocompleteAddress from "./AutocompleteAddress";
 import Cars from "./Cars";
 import Cards from "./Cards";
@@ -6,15 +45,23 @@ import { useRouter } from "next/navigation";
 import { SelectedCarAmountContext } from "@/context/SelectedCarAmountContext";
 
 function Booking() {
-  const screenHight = window.innerHeight * 0.8;
+  const [screenHeight, setScreenHeight] = useState<number>(0);  // Khởi tạo state cho screenHeight
   const { carAmount, setCarAmount } = useContext(SelectedCarAmountContext);
   const router: any = useRouter();
+
+  // Chỉ chạy sau khi component đã được mount trên client
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setScreenHeight(window.innerHeight * 0.8);  // Lấy giá trị window.innerHeight
+    }
+  }, []);  // Chạy chỉ một lần sau khi component mount
+
   return (
     <div className="p-5">
       <h2 className="text-[20px] font-semibold">Booking</h2>
       <div
         className="border-[1px] p-5 rounded-md"
-        style={{ height: screenHight }}
+        style={{ height: screenHeight }}  // Sử dụng giá trị screenHeight từ state
       >
         <AutocompleteAddress />
         <Cars />
